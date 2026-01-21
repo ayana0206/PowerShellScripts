b90a191099c8b1adbe04d9fe8697b4dffdbfe09a
ayana0206 <schwizerayana@gmail.com>
2026-01-21 13:44:52 +0100

    solved 4_If_Else.ps1

--- Uebungen_Scripting_MB01/Template/4_IF_Else.ps1
+++ Uebungen_Scripting_MB01/Template/4_IF_Else.ps1
@@ -3,3 +3,21 @@ a: - Überprüfe auf deinem Gerät ein Prozess mit dem Namen "OneNote" läuft un
 b: - Wenn der Prozess nicht läuft, gebe einen folgenden String aus "Prozess OneNote läuft nicht."
 c: - Überprüfe ob in deinem Scriptverzeichnis die Datei "Scriptlog.log" existiert. Falls nicht erstelle die Datei mit dem Inhalt "Script gestartet"
 #>
+
+$oneNoteProcess = Get-Process -Name "OneNote"
+
+if ($oneNoteProcess) {
+    Write-Output "Aufgabe a: $($oneNoteProcess.Id)"
+}
+else { # Aufgabe b:
+    Write-Output "Prozess OneNote läuft nicht."
+}
+
+# Aufgabe c:
+$logFile = Join-Path $PSScriptRoot "Scriptlog.log"
+
+if (-not (Test-Path $logFile)) {
+    "Script gestartet" | Out-File -FilePath $logFile
+} else {
+    Write-Output "Script da!"
+}
