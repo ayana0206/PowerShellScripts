3289f984f82be89c8a985ed8129738fc6e108643
ayana0206 <schwizerayana@gmail.com>
2026-01-21 14:26:50 +0100

    change directory

---
+++ Uebungen_Scripting_MB02/6_Function.ps1
@@ -0,0 +1,6 @@
+<#
+Übung 6a: - Schreibe eine Funktion die ein Logfile in deinem Übungsverzeichnis schreibt.
+            Als Parameter soll die Lognachricht, der Nachrichttyp(Info oder Error) mitgegeben werden.
+            Die Lognachricht soll folgendes Format haben: 22.08.2023 07:16 - Info - Das ist die Lognachricht
+Übung 6b: - Schreibe die Dateiennamen eures %TEMP% Verzeichnisses mit Hilfe der Funktion in das Logfile.
+#>
---
+++ Uebungen_Scripting_MB02/7_Module.ps1
@@ -0,0 +1,6 @@
+<#
+Übung 7a: - Erstelle ein Module Math mit den Funktionen "Sum", "Sub", "Div" und "Mul"
+             Als Beispiel fuer die Methode Mul soll folgender String returned werden: 3 * 9 = 27
+             Erstelle ein Script, in welchem das Module geladen wird und rufe alle Funktionen auf
+Übung 7b: - Gib alle Funktionen des Moduls Math auf die Konsole aus
+#>
