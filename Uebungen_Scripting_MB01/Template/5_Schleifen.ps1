02bd2b8eb3b2d84c7c7acedd1403284b70063298
ayana0206 <schwizerayana@gmail.com>
2026-01-21 14:22:44 +0100

    solved 5_Schleifen.ps1

--- Uebungen_Scripting_MB01/Template/5_Schleifen.ps1
+++ Uebungen_Scripting_MB01/Template/5_Schleifen.ps1
@@ -4,3 +4,23 @@ b: - Erstelle ein neues Array mit allen Files die grösser als 3MB sind. Speiche
 c: - Loope durch alle laufenden Prozesse und beende die Schlaufe beim ersten Prozess der im Namen mit einem O beginnt. 
             Gebe den Namen des Prozesses und Anzahl Durchgänge auf der Konsole aus.
 #>
+
+$files = Get-ChildItem "C:\Windows\System32" -File | Select-Object Name, Length
+
+Write-Output $files
+
+$bigFiles = Get-ChildItem "C:\Windows\System32" -File | Select-Object Name, Length | Where-Object { $_.Length -gt 3MB }
+
+Write-Output $bigFiles
+
+$count = 0
+
+foreach ($process in Get-Process) {
+    $count++
+    if ($process.Name.StartsWith("O")) {
+        
+        Write-Output "Prozessname: $($process.Name)"
+        Write-Output "Anzahl Durchgänge: $($count)"
+        break
+    }
+}
